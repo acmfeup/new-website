13003496fd0a73360e248a1ea0f0ede0540fc27e
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-09-16 00:11:35 +0100

    feat: added vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,45 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "ACM FEUP",
-	description: "Website for the ACM FEUP student chapter",
+  title: "ACM FEUP",
+  description: "Website for the ACM FEUP student chapter",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<Analytics />
-			<head>
-				<meta name="apple-mobile-web-app-title" content="ACM FEUP" />
-				<link rel="manifest" href="/manifest.json" />
-				<link rel="icon" href="/favicon.ico" sizes="any" />
-				<link rel="icon" href="/icon0.svg" type="image/svg+xml" />
-				<link rel="apple-touch-icon" href="/apple-icon.png" />
-			</head>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <Analytics />
+      <SpeedInsights />
+      <head>
+        <meta name="apple-mobile-web-app-title" content="ACM FEUP" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon0.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
