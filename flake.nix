2c7d9ff5c2df3cf16c7ddc070b75cb7b710d529f
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2026-04-20 11:29:52 +0100

    added flake.nix and updated dependencies

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "ACM FEUP DEV SHELL";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            pnpm
+          ];
+        };
+      });
+}
